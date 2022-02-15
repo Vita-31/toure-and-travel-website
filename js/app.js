b600367cabab4380eef918c469848168564131d4
Vita Kuznetsova <vita_kuznetsovav@gmail.com>
2022-02-15 21:19:29 +0200

    slider section

--- js/app.js
+++ js/app.js
@@ -15,3 +15,14 @@ scenes.forEach(scene => {
     var parallaxInstance = new Parallax(scene); 
 })
 
+//swiper 
+// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
